faa18f7ace7eed382f6005350a4286eabe27650e
Artnaa <artnaa3@gmail.com>
2023-08-14 22:08:24 +0300

    input type change

--- src/Join_form/join.js
+++ src/Join_form/join.js
@@ -4,24 +4,24 @@ import './join.css'
 
 
 export function joinForm() {
-  return(
+  return (
     <form name='formSection'>
       <div className="join_form_title">
         <h4 className='bol_dtext_title'>JOIN HYDRA</h4>
-        <img src={joinvector} alt="vector"/>
+        <img src={joinvector} alt="vector" />
         <h5 className='thin_text_subtitle'>Let’s Build Your VR Experience</h5>
       </div>
 
       <div className="users_first_last_name">
-        <input type="text" name="" value="First Name"/>
-        <input type="text" name="" value="Last Name"/>
+        <input type="text" name="" placeholder='First Name' />
+        <input type="text" name="" placeholder='Last Name' />
       </div>
 
       <div className="users_contact">
-        <input type="text" name="" value="Email"/>
-        <input type="text" name="" value="Phone Number"/>
+        <input type="text" name="" placeholder='Email' />
+        <input type="text" name="" placeholder='Phone Number' />
       </div>
-      <input type="text" name="subject" value="Subject" className='subject_input'/>
+      <input type="text" name="subject" placeholder='Subject' className='subject_input' />
       <textarea placeholder='textarea'></textarea>
     </form>
   )
